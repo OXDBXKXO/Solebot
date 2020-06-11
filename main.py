1d1140cb7ba2c56f55c14f0b8ded6247b3438f61
Louis Chevalier <louis.chevalier@epita.fr>
2020-06-11 22:05:28 +0200

    Put all variables at the beginning of the file to ease modification and fixed misued Requet constructor

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from lib import Requet, Log
 
 
+#CSRF bypass
 dwsid = "g3RwuW3Z1eOFTltJ16egKtIbUYeWEcvLChCJ6A9zJWmPgUqO1KkQlLo2Oe8AyQsijW9jrXZgzYgQoitvjPnSeg=="
 csrf_token = "sXAsYsdV_HVPif8NdXb6DY1FlXR6gyXacL7w7BgpdlAVgkqMM0gIhBAIkyw7YJjtBE721X68VyEFlLCi6sMZ6pGziBvYUW_s1prQb6o4qHKNc4U1jvKg0JStiTw1FHWCwaBWdbKODgMu10Mbl_jqPqt4HqlJAy9rzIwNHejzEgZaatei-Dc%3D"
 
+#Credentials
 email = "test10@test.test"
 password = "TESTtest123"
+
+#Product info
+pid = "0181863000000008"
+size = "44"
+
+#Debug mode (True/False)
+debug = False
+
 
 
 def solebox_create_user(dwsid, csrf_token, email, password, debug):
@@ -13,9 +23,9 @@ def solebox_create_user(dwsid, csrf_token, email, password, debug):
     Tries to create a new user with given arguments, returns success as boolean
     '''
     mail = email.replace('@', '%40')
-    req = Requet(debug, 'www.solebox.com')
+    req = Requet(True, 'www.solebox.com')
 
-    req.debug = True
+    req.debug = debug
     req.useragent = 'Mozilla/5.0 (X11; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0'
 
     rep = req.requet('/on/demandware.store/Sites-solebox-Site/en_FR/Account-SubmitRegistration?rurl=1&format=ajax',
@@ -58,9 +68,9 @@ def solebox_login(dwsid, csrf_token, email, password, debug):
     Tries to login with given arguments, returns success as a dictionary, either None or containing session cookies
     '''
     mail = email.replace('@', '%40')
-    req = Requet(debug, 'www.solebox.com')
+    req = Requet(True, 'www.solebox.com')
 
-    req.debug = True
+    req.debug = debug
     req.useragent = 'Mozilla/5.0 (X11; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0'
 
     rep, cookies = req.requet2('/en_FR/authentication?rurl=1&format=ajax',
@@ -105,9 +115,9 @@ def solebox_buy_shoe(pid, size, dwsid, debug):
     '''
     Tries to add a product with given pied and size, returns success as boolean
     '''
-    req = Requet(debug, 'www.solebox.com')
+    req = Requet(True, 'www.solebox.com')
 
-    req.debug = True
+    req.debug = debug
     req.useragent = 'Mozilla/5.0 (X11; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0'
 
     rep = req.requet('/en_FR/add-product?format=ajax',
@@ -130,10 +140,16 @@ def solebox_buy_shoe(pid, size, dwsid, debug):
     return "\"error\": false" in rep
 
 
-cookies = None
 
-solebox_create_user(dwsid, csrf_token, email, password, True)
-cookies = solebox_login(dwsid, csrf_token, "test10@test.test", "TESTtest123", True)
+if solebox_create_user(dwsid, csrf_token, email, password, debug):
+    print("An error occured while creating new user")
+
+cookies = solebox_login(dwsid, csrf_token, "test10@test.test", "TESTtest123", debug)
 
 if cookies is not None:
-    solebox_buy_shoe('0181863000000008', '44', cookies["dwsid"], True)
+    if solebox_buy_shoe(pid, size, cookies["dwsid"], debug):
+        print("Product added successfuly")
+    else:
+        print("An error occured while adding product to cart")
+else:
+    print("An error occured during connection")
