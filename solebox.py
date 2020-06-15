fb62c98caaf05d9205662190b1282a8544d7bc4a
Louis Chevalier <louis.chevalier@epita.fr>
2020-06-15 17:00:59 +0200

    cleaned request bodies

--- solebox.py
+++ solebox.py
@@ -37,7 +37,19 @@ def solebox_create_user(dwsid, csrf_token, email, password, debug):
         _px3=85862b021d9e3c2e1e7ea0150446fa64d5b6113b5a0a6bc9284a8313451989ba:hR7FFjV0tJkH+Ilmm9ERxroRkskQ4YtKb3DqS/e9tkExuhV5lIaIv+2Kx9aDwJNcYhgbNUhMA04VJrYsw+ykwQ==:1000:f0SZHiA3SXzKhQQgAkk0AMD/7XYtfex5gQ7ojHhz4Fa29fLgmFKi/gMhBOTeh9JZ9+7Fcnd35tZWBL9v26lndmKw6LupvMYO2bGRPn6vEGAULteg4W2U76OUAdzKUGUUwfHumZ2XoaLYwhBGynoM/9aOV9yW1KmMVVzg926m+0A=; \
         _uetsid=0e9cb852-3f68-4237-5167-c60b20aea97b; \
         _uetvid=dd4f966a-dec0-14b2-9d5c-fea46686f6fe',
-    	body='dwfrm_profile_register_title=mr&dwfrm_profile_register_firstName=TEST&dwfrm_profile_register_lastName=TEST&dwfrm_profile_register_email=' + mail + '&dwfrm_profile_register_emailConfirm=' + mail + '&dwfrm_profile_register_password=' + password + '&dwfrm_profile_register_passwordConfirm=' + password + '&dwfrm_profile_register_phone=&dwfrm_profile_register_birthday=&dwfrm_profile_register_acceptPolicy=true&csrf_token=' + csrf_token
+
+
+    	body='dwfrm_profile_register_title=mr&\
+        dwfrm_profile_register_firstName=TEST&\
+        dwfrm_profile_register_lastName=TEST&\
+        dwfrm_profile_register_email=' + mail + '&\
+        dwfrm_profile_register_emailConfirm=' + mail + '&\
+        dwfrm_profile_register_password=' + password + '&\
+        dwfrm_profile_register_passwordConfirm=' + password + '&\
+        dwfrm_profile_register_phone=&\
+        dwfrm_profile_register_birthday=&\
+        dwfrm_profile_register_acceptPolicy=true&\
+        csrf_token=' + csrf_token
     )
 
     if debug:
@@ -82,7 +94,11 @@ def solebox_login(dwsid, csrf_token, email, password, debug):
         _px3=85862b021d9e3c2e1e7ea0150446fa64d5b6113b5a0a6bc9284a8313451989ba:hR7FFjV0tJkH+Ilmm9ERxroRkskQ4YtKb3DqS/e9tkExuhV5lIaIv+2Kx9aDwJNcYhgbNUhMA04VJrYsw+ykwQ==:1000:f0SZHiA3SXzKhQQgAkk0AMD/7XYtfex5gQ7ojHhz4Fa29fLgmFKi/gMhBOTeh9JZ9+7Fcnd35tZWBL9v26lndmKw6LupvMYO2bGRPn6vEGAULteg4W2U76OUAdzKUGUUwfHumZ2XoaLYwhBGynoM/9aOV9yW1KmMVVzg926m+0A=; \
         _uetsid=0e9cb852-3f68-4237-5167-c60b20aea97b; \
         _uetvid=dd4f966a-dec0-14b2-9d5c-fea46686f6fe',
-    	body='dwfrm_profile_customer_email=' + mail + '&dwfrm_profile_login_password=' + password + '&csrf_token=' + csrf_token
+
+
+    	body='dwfrm_profile_customer_email=' + mail + '&\
+        dwfrm_profile_login_password=' + password + '&\
+        csrf_token=' + csrf_token
     )
 
     if debug:
@@ -113,7 +129,10 @@ def solebox_buy_shoe(pid, size, dwsid, debug):
         cookies='test; __cfduid=d123ae9b9fdbba5d75931bf63487ad7961591874808; \
         dwanonymous_0e5f1b8bd4b7e281cbecc26270bd55c1=acZxdpaVm51ROHfzyF0BZEKdAO; \
         _pxhd=28aa1be4c068a34c59e9a71dbcea4ed4e32ad618c39d529b5428a3fcda8f4e93:76d987a1-abd6-11ea-95dd-3587cd26823e; _gcl_au=1.1.562668024.1591874820; customerCountry=fr; _ga=GA1.2.1607499967.1591874823; _gid=GA1.2.2019216406.1591874823; _pxvid=76d987a1-abd6-11ea-95dd-3587cd26823e; _fbp=fb.1.1591874830153.1517504993; dwsid=' + dwsid + '; dwac_6915a153f1e2381a3decf47a04=ZbxwRG86yw36dFxhUsdrEMrh3wo_J5Eknsc%3D|dw-only|||EUR|false|Europe%2FBerlin|true; sid=ZbxwRG86yw36dFxhUsdrEMrh3wo_J5Eknsc; __cq_dnt=1; dw_dnt=1; test; hideLocalizationDialog=true; acceptCookie=true; _uetsid=0e9cb852-3f68-4237-5167-c60b20aea97b; _uetvid=dd4f966a-dec0-14b2-9d5c-fea46686f6fe',
-    	body='pid=' + pid + '&options=%5B%7B%22optionId%22%3A%22212%22%2C%22selectedValueId%22%3A%22' + size +'%22%7D%5D&quantity=1'
+
+
+    	body='pid=' + pid + '&\
+        options=%5B%7B%22optionId%22%3A%22212%22%2C%22selectedValueId%22%3A%22' + size +'%22%7D%5D&quantity=1'
     )
 
     if debug:
